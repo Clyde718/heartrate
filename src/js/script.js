5b7b56ab7de9918fbebc133923f7de8776996635
Kiril <kiril.bogdan.97@gmail.com>
2020-06-17 00:37:40 +0300

    Initial Commit

--- src/js/script.js
+++ src/js/script.js
@@ -83,4 +83,25 @@ $(document).ready(function () {
 
 
 
+  // mailer
+  $('form').submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function () {
+      $(this).find('input').val('');
+      $('#consultation, #order').fadeOut();
+      $('#thanks').fadeIn();
+
+
+
+      $('form').trigger('reset');
+    });
+    return false;
+  })
+
+
+
 });
